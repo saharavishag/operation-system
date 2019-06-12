f1548318d10975f066ffc2626ceb15e966ca7a59
nadav <nadavbr@dnabits.com>
2019-06-12 09:35:38 -0400

    lsnd

--- Makefile
+++ Makefile
@@ -183,6 +183,7 @@ UPROGS=\
 	_wc\
 	_zombie\
 	_sanity\
+	_lsnd\
 
 fs.img: mkfs README $(UPROGS)
 	./mkfs fs.img README $(UPROGS)
---
+++ lsnd.c
@@ -0,0 +1,65 @@
+//
+// Created by nadav and avishag on 6/12/19.
+//
+#include "types.h"
+#include "stat.h"
+#include "user.h"
+#include "fs.h"
+
+void getData(char *buf, char *s) {
+    char *temp = s;
+    char *s1, *s2;
+    int len = 0;
+
+
+    for (int i = 0; i < 7; ++i) {
+
+        s1 = strchr(buf, ':') + 2;
+        s2 = strchr(buf, '\n');
+        len = (int) s2 - (int) s1;
+
+        if (i == 0 || i == 1) {
+            memmove(temp + 1, s1, len);
+            *temp = '#';
+            len++;
+        } else {
+            memmove(temp, s1, len);
+        }
+        buf = s2 + 1;
+        temp += len;
+
+        *temp = ' ';
+        temp++;
+    }
+    *temp = '\0';
+}
+
+
+int main(void) {
+    int inodesDirFd = open("/proc/inodeinfo", 0);
+    struct dirent de;
+    char path[64];
+    char buf[512];
+    char result[512];
+
+    while (read(inodesDirFd, &de, sizeof(de)) > 0) {
+        printf(1, "name: %s inum: %d\n", de.name, de.inum);
+        sprintf(path, "/proc/inodeinfo/%s", de.name);
+        int inodeFd = open(path, 0);
+        if (read(inodeFd, &buf, sizeof(buf)) <= 0) {
+            printf(1, "reading name: %s inum: %d failed\n", de.name, de.inum);
+        }
+        getData(buf, result);
+        printf(1, "result: %s\n", result);
+
+
+        close(inodeFd);
+
+    }
+    close(inodesDirFd);
+
+
+    exit();
+}
+
+
